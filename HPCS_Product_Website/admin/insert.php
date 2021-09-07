dfcddb9402165e8f25550f257e6d3c2983ecc50b
Akanksha Bankhele <80443183+AkankshaBankhele@users.noreply.github.com>
2021-09-07 17:44:40 +0530

    Update insert.php

--- HPCS_Product_Website/admin/insert.php
+++ HPCS_Product_Website/admin/insert.php
@@ -4,24 +4,27 @@
 
 require_once('../config/config.php');
 
+
   $name = filter_input(INPUT_POST, 'name');
-  $platf = $_POST["platf"];
-  $category = $_POST["category"];
-    $validity = $_POST["validity"];
-  $price = $_POST["price"];
-  $currency = $_POST["currency"];
-  $productblog = $_POST["productblog"];
-  $productvideo = $_POST["productvideo"];
-  $productmql = $_POST["productmql"];
-  $description = $_POST["description"];
-  $descriptioninput = $_POST["descriptioninput"];
-  $tags = $_POST["tags"];
+  $platf = filter_input(INPUT_POST, 'platf');
+
+  $category =filter_input(INPUT_POST, 'category'); 
+    $validity = filter_input(INPUT_POST, 'validity');
+  $price = filter_input(INPUT_POST, 'price');
+  $currency =filter_input(INPUT_POST, 'currency');
+  $productblog =filter_input(INPUT_POST, 'productblog');
+  $productvideo = filter_input(INPUT_POST, 'productvideo');
+  $productmql = filter_input(INPUT_POST, 'productmql');
+  $description =filter_input(INPUT_POST, 'description'); 
+  $descriptioninput =filter_input(INPUT_POST, 'descriptioninput'); 
+  $tags = filter_input(INPUT_POST, 'tags');
   // $discount = $_POST["discount"];
-  $image = $_POST["image"];
-  $iguide = $_POST["iguide"];
-  $userguide = $_POST["userguide"];
-  $productfile = $_POST["productfile"];
+  $image =filter_input(INPUT_POST, 'image'); 
+  $iguide =filter_input(INPUT_POST, 'iguide'); 
+  $userguide =filter_input(INPUT_POST, 'userguide'); 
+  $productfile = filter_input(INPUT_POST, 'productfile');
  
+
 
   $sql = "INSERT INTO `product`(`category`, `name`,`validity`, `cost`,`currency`, `image`, `userguide`, `description`, `input_description`, `tags`, `Platform`, `productfile`, `link_blog`, `link_video`, `link_mql5`, `installation_guide`) VALUES ('".$category."','".$name."','".$validity."','".$price."','".$currency."','".$image."','".$userguide."','".$description."','".$descriptioninput."','".$tags."','".$platf."','".$productfile."','".$productblog."','".$productvideo."','".$productmql."','".$iguide."') ";
   $result=$conn->query($sql);
