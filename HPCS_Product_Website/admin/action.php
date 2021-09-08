728041758b46a0547a254035337ef1fc07f06d6c
Akanksha Bankhele <80443183+AkankshaBankhele@users.noreply.github.com>
2021-09-08 15:21:11 +0530

    Update action.php

--- HPCS_Product_Website/admin/action.php
+++ HPCS_Product_Website/admin/action.php
@@ -2,16 +2,16 @@
 //action.php
 include "../config/config.php";
 
-if($_POST['action']=="delete"){
+if(filter_input(INPUT_POST, 'action')=="delete"){
 
-    $id = $_POST['plt_id'];
+    $id = filter_input(INPUT_POST, 'plt_id');
     $del="DELETE FROM `platform` WHERE plt_id=$id";
     $delete = mysqli_query($conn,$del);
 
-}elseif($_POST['action']=="update"){
+}elseif(filter_input(INPUT_POST, 'action')=="update"){
 
-    $id = $_POST['plt_id'];
-    $plt_name = $_POST['plt_name'];
+    $id = filter_input(INPUT_POST, 'plt_id');
+    $plt_name =filter_input(INPUT_POST, 'plt_name'); 
     $upt="UPDATE `platform` SET `plt_name`='".$plt_name."' WHERE plt_id=$id";
     $update = mysqli_query($conn,$upt);
 
