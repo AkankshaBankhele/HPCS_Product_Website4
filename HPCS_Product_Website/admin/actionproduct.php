f954245485565993d45a665ba0023edb92640749
Akanksha Bankhele <80443183+AkankshaBankhele@users.noreply.github.com>
2021-09-08 18:34:55 +0530

    Update actionproduct.php

--- HPCS_Product_Website/admin/actionproduct.php
+++ HPCS_Product_Website/admin/actionproduct.php
@@ -2,9 +2,9 @@
 //action.php
 include "../config/config.php";
 
-if($_POST['action']=="delete"){
+if(filter_input(INPUT_POST, 'action')=="delete"){
 
-    $id = $_POST['sl'];
+    $id = filter_input(INPUT_POST, 'sl');
   
     $del="DELETE FROM `product` WHERE sl=$id";
     $delete = mysqli_query($conn,$del);
