1fcd3b438c7a3e2683bd3d851fe9c1a9c31da461
Akanksha Bankhele <80443183+AkankshaBankhele@users.noreply.github.com>
2021-09-08 17:05:33 +0530

    Update checktag.php

--- HPCS_Product_Website/admin/checktag.php
+++ HPCS_Product_Website/admin/checktag.php
@@ -2,24 +2,23 @@
   date_default_timezone_set("Asia/Calcutta");   
 
 require_once('../config/config.php');
-  $tag_name = $_POST["tag_name"];
-
+  $tag_name = filter_input(INPUT_POST, 'tag_name');
  
   $sql = "SELECT * FROM `tag` WHERE tag_name='$tag_name'";
   $result=mysqli_query($conn,$sql);
         if( mysqli_num_rows($result)==1){
             $row=mysqli_fetch_assoc($result);
             
-            if($row["tag_name"]==$_POST["tag_name"] )
+            if($row["tag_name"]==filter_input(INPUT_POST, 'tag_name') )
             {
                $tg=$row["tag_name"];
-               echo $tg;
+               print_r($tg);
             }
            
         }
          else
             {
-               echo "2";
+               print_r("2");
             }
              
 ?>
