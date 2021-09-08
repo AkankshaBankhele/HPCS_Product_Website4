8f16357495ede61f5196d59c15295436b62fb221
Akanksha Bankhele <80443183+AkankshaBankhele@users.noreply.github.com>
2021-09-08 12:43:06 +0530

    Update actiontag.php

--- HPCS_Product_Website/admin/actiontag.php
+++ HPCS_Product_Website/admin/actiontag.php
@@ -2,18 +2,18 @@
 //action.php
 include "../config/config.php";
 
-if($_POST['action']=="delete"){
+if(filter_input(INPUT_POST, 'action')=="delete"){
 
-    $id = $_POST['tag_id'];
+    $id = filter_input(INPUT_POST, 'tag_id');
   
     $del="DELETE FROM `tag` WHERE tag_id=$id";
     $delete = mysqli_query($conn,$del);
    
 
-}elseif($_POST['action']=="update"){
+}elseif(filter_input(INPUT_POST, 'action')=="update"){
 
-    $id = $_POST['tag_id'];
-    $tag_name = $_POST['tag_name'];
+    $id = filter_input(INPUT_POST, 'tag_id');
+    $tag_name = filter_input(INPUT_POST, 'tag_name');
     $upt="UPDATE `tag` SET `tag_name`='".$tag_name."' WHERE tag_id=$id";
     $update = mysqli_query($conn,$upt);
 
